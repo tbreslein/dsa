06b4c7542ed2e672ae257b4065d906449947a40e
tbreslein <tommy.breslein@protonmail.com>
2023-09-09 20:24:34 +0200

    formatting

--- ocaml_dsa/lib/slist/slist.mli
+++ ocaml_dsa/lib/slist/slist.mli
@@ -1,14 +1,14 @@
-type 'e t = Nil | Node of 'e * 'e t [@@deriving show, eq]
+type 'e t = Nil | Node of 'e * 'e t [@@deriving show, eq];;
 
-exception Empty
-exception TooShort
-exception TooLong
+exception Empty;;
+exception TooShort;;
+exception TooLong;;
 
-val empty : 'e t -> bool
-val length : 'e t -> int
-val head : 'e t -> 'e
-val tail : 'e t -> 'e t
-val take : int -> 'e t -> 'e t
-val drop : int -> 'e t -> 'e t
-val push : 'e t -> 'e -> 'e t
-val pop : 'e t -> 'e * 'e t
+val empty : 'e t -> bool;;
+val length : 'e t -> int;;
+val head : 'e t -> 'e;;
+val tail : 'e t -> 'e t;;
+val take : int -> 'e t -> 'e t;;
+val drop : int -> 'e t -> 'e t;;
+val push : 'e t -> 'e -> 'e t;;
+val pop : 'e t -> 'e * 'e t;;
